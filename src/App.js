109360c3a1fa98aaf5aca64be436061f62ce47d4
Ziad Hegazi <ziadhegazi@hotmail.com>
2021-05-08 00:04:31 +0200

    State Checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from "./ZH style 2 WIP3pdf.jpg"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       message: "Show Profile",
+       shows: false
+    }
+  }
+
+  // Defining person
+  person = {
+    fullname: this.props.fullname,
+    bio: this.props.bio,
+    profession: this.props.profession
+  }
+
+
+  // Button function
+  buttonclick = () => {
+    const profile = document.querySelector(".profile");
+    if (this.state.shows === false) {
+      this.setState({
+        message: "Hide Profile",
+        shows: true,
+      });
+      profile.style.display = "contents"
+    }else {
+      this.setState({
+        message: "Show Profile",
+        shows: false
+      });
+      profile.style.display = "none"
+    }
+  };
+
+  // rendering content
+  render() {
+    return (
+      <div>
+        <div className="profile">
+          <img src={logo} alt="Logo" className="logo"></img>
+          <h3>{this.person.fullname}</h3>
+          <h4>{this.person.profession}</h4>
+          <p>{this.person.bio}</p>
+        </div>
+        <button type="button" onClick={this.buttonclick}>{this.state.message}</button>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App 
+      fullname={"Ziad Hegazi"} 
+      bio={"I am fluent in English and an easy going person with great communication skills. I also am great with computers and technology and fixing problems. I self-teach myself some new skills frequently like new programming languages, and skills to use. Well motivated and self-disciplined driven by the motive of always improving myself constantly. "} 
+      profession={"Full Stack Developer"}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
